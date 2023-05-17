9ae7af44a1111dbfce73c1d5101797eb75efd75d
Lakshamana <lakshamana@protonmail.com>
2023-05-17 16:55:52 -0300

    add docs related files

---
+++ lib/greeter.ex
@@ -0,0 +1,23 @@
+defmodule Greeter do
+  @moduledoc """
+  Provides a funcion to `hello/1` a person.
+  """
+
+  @doc """
+  Prints a hello message.
+
+  ## Parameters
+  - `name` is the name of the person to greet.
+
+  ## Examples
+    iex> Greeter.hello("lakshamana")
+    "Hello, lakshamana"
+
+    iex> Greeter.hello("Peter")
+    "Hello, Peter"
+  """
+  @spec hello(String.t()) :: String.t()  # type annotation
+  def hello(name) do
+    "Hello, #{name}"
+  end
+end
