de7bdc9fa54443c248ce58936e08b4d012c637f2
Lakshamana <lakshamana@protonmail.com>
2023-09-18 00:06:02 -0300

    chore: update deps, add escript build

--- mix.exs
+++ mix.exs
@@ -1,21 +1,8 @@
 defmodule ElixirExercises.MixProject do
   use Mix.Project
 
-  def project do
-    [
-      app: :elixir_exercises,
-      version: "0.1.0",
-      elixir: "~> 1.14",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
-    ]
-  end
-
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger]
-    ]
+  defp escript do
+    [main_module: ElixirExercises.CLI]
   end
 
   # Run "mix help deps" to learn about dependencies.
@@ -24,9 +11,26 @@ defmodule ElixirExercises.MixProject do
       {:phoenix, "~> 1.1 or ~> 1.2"},
       {:phoenix_html, "~> 2.3"},
       {:cowboy, "~> 1.0", only: [:dev, :test]},
-      {:slime, "~> 0.14"},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:tzdata, "~> 1.1"},
+    ]
+  end
+
+  def project do
+    [
+      app: :elixir_exercises,
+      version: "0.1.0",
+      elixir: "~> 1.14",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      escript: escript()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
     ]
   end
 end
